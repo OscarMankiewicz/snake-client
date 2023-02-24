80442c5efd267fc942acf9ac0c3b52c24516d250
Oscar <omankiewicz77@gmail.com>
2023-02-24 10:22:55 -0500

    added a setupInput function and a handleUserInput function to make commands for the snake later

--- play.js
+++ play.js
@@ -1,2 +1,21 @@
 const client = require('./client')
+
+
+const setupInput = function () {
+    const stdin = process.stdin;
+    stdin.setRawMode(true);
+    stdin.setEncoding("utf8");
+    stdin.resume();
+    stdin.on("data", handleUserInput);
+    return stdin;
+};
+
+const handleUserInput = function (data) {
+    // your code here
+    if(data === '\u0003') {
+        process.exit();
+    }
+};
+
 client.connect();
+setupInput();
