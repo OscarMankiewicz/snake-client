f4bda1d6a826b8ce2dfc665bebda116543d798b9
Oscar <omankiewicz77@gmail.com>
2023-02-24 11:40:06 -0500

    changed handleUserInput function to write to server

--- input.js
+++ input.js
@@ -1,4 +1,7 @@
-const setupInput = function () {
+let connection;
+
+const setupInput = (conn) => {
+    connection = conn;
     const stdin = process.stdin;
     stdin.setRawMode(true);
     stdin.setEncoding("utf8");
@@ -11,6 +14,14 @@ const handleUserInput = function (data) {
     // your code here
     if(data === '\u0003') {
         process.exit();
+    } else if (data === 'w') {
+        connection.write('Move: up')
+    } else if (data === 'a') {
+        connection.write('Move: left')
+    } else if (data === 's') {
+        connection.write('Move: down')
+    } else if (data === 'd') {
+        connection.write('Move: right')
     }
 };
 
