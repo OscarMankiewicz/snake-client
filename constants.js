88acb879a6ac8d935cd249b2c81d5af43c31d639
Oscar <omankiewicz77@gmail.com>
2023-02-25 10:58:54 -0500

    made a file to store all constants

---
+++ constants.js
@@ -0,0 +1,21 @@
+const IP = 'localhost';
+const PORT = 50541;
+
+const inputKeys = {
+    MOVE_UP_KEY: 'w',
+    MOVE_LEFT_KEY: 'a',
+    MOVE_DOWN_KEY: 's',
+    MOVE_RIGHT_KEY: 'd',
+    SAY_WOW_KEY: '1',
+    SAY_GG_KEY: '2',
+    SAY_HAHA_KEY: '3',
+    SAY_LETS_PLAY_AGAIN_KEY: '4',
+    SAY_SURE_KEY: '5',
+    SAY_NO_THANKS_KEY: '6',
+};
+
+module.exports = {
+    inputKeys,
+    IP,
+    PORT,
+};
